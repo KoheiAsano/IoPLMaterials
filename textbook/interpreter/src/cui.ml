bcdaad333dbeb462a4439e583c36f2dacc630b05
KoheiAsano <kk.asano.luxy@gmail.com>
2020-05-24 16:52:42 +0900

    2, 3, 4 and back to repl

--- textbook/interpreter/src/cui.ml
+++ textbook/interpreter/src/cui.ml
@@ -3,14 +3,22 @@ open Eval
 let rec read_eval_print env =
   print_string "# ";
   flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_decl env decl in
-  Printf.printf "val %s = " id;
-  pp_val v;
-  print_newline();
-  read_eval_print newenv
+  try 
+    let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+    let (id, newenv, v) = eval_decl env decl in
+    Printf.printf "val %s = " id;
+    pp_val v;
+    print_newline();
+    read_eval_print newenv;
+  with 
+  | Error (s) -> Printf.printf "%s\n" s;read_eval_print env;
+  | _ -> Printf.printf "Fetal error \n";read_eval_print env
+
 
 let initial_env =
   Environment.extend "i" (IntV 1)
+  (Environment.extend "ii" (IntV 2)
+  (Environment.extend "iii" (IntV 3)
+  (Environment.extend "iv" (IntV 4)
     (Environment.extend "v" (IntV 5)
-       (Environment.extend "x" (IntV 10) Environment.empty))
+       (Environment.extend "x" (IntV 10) Environment.empty)))))
